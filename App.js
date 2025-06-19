6d92eed99d395079ae5b4d32ec42f90e537face5
Olga Kozlova <olgakozlova.web@gmail.com>
2025-06-19 20:56:20 +0300

    eslint setup

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
 
+/**
+ * Main component representing the React Native application.
+ * Renders a basic structure with a status bar and a body containing app content.
+ *
+ * @return {JSX.Element} The component JSX containing the app layout.
+ */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.statusbar}>
+          <Text>Our react native app</Text>
+        </View>
+        <View style={styles.body}>
+          <Text>Our react native app 4</Text>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'orange',
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  statusbar: {
+    padding: 16,
+    backgroundColor: 'green',
+  },
+  body: {
+    padding: 16,
+    backgroundColor: 'blue',
+    flex: 1,
   },
 });
